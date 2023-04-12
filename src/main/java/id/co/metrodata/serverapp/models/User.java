3c54b4306800d2b0e7756471f6302e31b8689f86
fuzi <fzein.1695@gmail.com>
2023-04-12 09:40:11 +0700

    one to one - employee to user

---
+++ src/main/java/id/co/metrodata/serverapp/models/User.java
@@ -0,0 +1,37 @@
+package id.co.metrodata.serverapp.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+  @Id
+  private Integer id;
+
+  @Column(unique = true, nullable = false)
+  private String username;
+
+  @Column(nullable = false)
+  private String password;
+
+  private Boolean isEnabled = true;
+  private Boolean isAccountNonLocked = true;
+
+  @OneToOne
+  @MapsId
+  @JoinColumn(name = "id")
+  private Employee employee;
+}
